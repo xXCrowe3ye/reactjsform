6ddb3cee416d831e93f79028a4042a23a016e2ea
SpikeSpiegel <hanzangelobernabe212@gmail.com>
2022-05-23 20:34:11 +0800

    first commit

---
+++ src/Form.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Form.css";
+import "./script.js";
+
+
+function Form() {
+  return (
+
+    <body>
+ <div class="container">
+    <h1>REGISTER</h1>
+    <form id="registration_form">
+        <div>
+            <input type="text" id="form_fname" name="" required=""></input>
+            <span class="error_form" id="fname_error_message"></span>
+            <label>
+					First Name
+				</label>
+        </div>
+        <div>
+            <input type="text" id="form_sname" name="" required=""></input>
+            <span class="error_form" id="sname_error_message"></span>
+            <label>
+					Second Name
+				</label>
+        </div>
+        <div>
+            <input type="email" id="form_email" name="" required=""></input>
+            <span class="error_form" id="email_error_message"></span>
+            <label>Email id</label>
+        </div>
+        <div>
+            <input type="password" id="form_password" name="" required=""></input>
+            <span class="error_form" id="password_error_message"></span>
+            <label>Password</label>
+        </div>
+        <div>
+            <input type="password" id="form_retype_password" name="" required=""></input>
+            <span class="error_form" id="retype_password_error_message"></span>
+            <label>Confirm Password</label>
+        </div>
+        <input type="submit" value="Register" name=""></input>
+    </form>
+    </div>
+    <script src="script.js"></script>
+
+    </body>
+  );
+}
+
+export default Form;
